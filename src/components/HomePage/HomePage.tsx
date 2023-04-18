5f7d9b2c57d016e09934c69edb68c16671d96e77
akatzav <avigailkatzav@gmail.com>
2023-04-18 19:27:58 +0300

    images

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ export const HomePage = () => {
 
     return (
         <div className={css.homePage}>
+            <br /><br />
             <h1 className={css.title}>France.</h1>
             <br /><br /><br /><br /><br /><br />
             <p className={css.description}>
