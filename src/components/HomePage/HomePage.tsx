25cc45af95fdd11b2ae293d89cba2b77d55b0455
akatzav <avigailkatzav@gmail.com>
2023-04-18 19:15:19 +0300

    firsts level

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import css from './HomePage.module.scss'
+
+export const HomePage = () => {
+
+    return (
+        <div className={css.homePage}>
+            <h1 className={css.title}>France.</h1>
+            <br /><br /><br /><br /><br /><br />
+            <p className={css.description}>
+                The Ideal Place
+            </p>
+        </div>
+    )
+}
